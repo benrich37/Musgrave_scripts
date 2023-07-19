76eb1c3b45fd8c9d3f1afdadd57d98d6904374af
Ben Rich <beri9208@colorado.edu>
2023-07-19 13:34:08 -0600

    Added pbcs to created log files

--- BenRich/right_click_scripts/traj_to_log.py
+++ BenRich/right_click_scripts/traj_to_log.py
@@ -4,14 +4,16 @@
 import sys
 file = sys.argv[1]
 from ase.io.trajectory import TrajectoryReader
+import numpy as np
 
 assert ".traj" in file
 
 def traj_to_log_str(traj):
     dump_str = "\n Entering Link 1 \n \n"
     nSteps = len(traj)
+    do_cell = np.sum(abs(traj[0].cell)) > 0
     for i in range(nSteps):
-        dump_str += log_input_orientation(traj[i])
+        dump_str += log_input_orientation(traj[i], do_cell=do_cell)
         dump_str += scf_str(traj[i])
         dump_str += opt_spacer(i, nSteps)
     dump_str += log_input_orientation(traj[-1])
@@ -30,7 +32,7 @@ def opt_spacer(i, nSteps):
     dump_str += "\n GradGradGradGradGradGradGradGradGradGradGradGradGradGradGradGradGradGrad\n"
     return dump_str
 
-def log_input_orientation(atoms):
+def log_input_orientation(atoms, do_cell=False):
     dump_str = "                          Input orientation:                          \n"
     dump_str += " ---------------------------------------------------------------------\n"
     dump_str += " Center     Atomic      Atomic             Coordinates (Angstroms)\n"
@@ -38,11 +40,19 @@ def log_input_orientation(atoms):
     dump_str += " ---------------------------------------------------------------------"
     at_ns = atoms.get_atomic_numbers()
     at_posns = atoms.positions
-    for i in range(len(at_posns)):
+    nAtoms = len(at_ns)
+    for i in range(nAtoms):
         dump_str += f" {i+1} {at_ns[i]} 0 "
         for j in range(3):
             dump_str += f"{at_posns[i][j]} "
         dump_str += "\n"
+    if do_cell:
+        cell = atoms.cell
+        for i in range(3):
+            dump_str += f"{i + nAtoms + 1} -2 0 "
+            for j in range(3):
+                dump_str += f"{cell[i][j]} "
+            dump_str += "\n"
     dump_str += " ---------------------------------------------------------------------\n"
     return dump_str
 
